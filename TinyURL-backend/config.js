998267056500cb785905eb111c0c27fefe74ef8e
Joey Chen <joeyshchen@gmail.com>
2021-01-11 15:28:11 -0800

    added a config file with hostname

---
+++ TinyURL-backend/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    hostname: "localhost:8000" 
+    // prod: "http://ec2-3-135-231-41.us-east-2.compute.amazonaws.com"
+}
--- TinyURL-backend/services/TinyURLService.js
+++ TinyURL-backend/services/TinyURLService.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 import mongoose from 'mongoose';
 import { tinyURLSchema } from '../models/tinyURL';
-import os from 'os';
+import {hostname} from '../config';
 // import cache, { clearKey } from './cache';
 
 const tinyURL = mongoose.model('tinyURL', tinyURLSchema);
@@ -37,7 +37,7 @@ export const createTinyURL = async (newTinyURL) =>{
     console.log(urlData);
     
     //if user didn't opt to create unique short link, create one for them
-    let shortL = `${window.location.hostname}/tiny/`;
+    let shortL = `${hostname}/tiny/`;
     let genId = shortid.generate();
 
     if(newTinyURL.shortLink){
